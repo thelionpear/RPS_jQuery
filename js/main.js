1ae355a8f403fefb09c645830594a56b08c3da73
Michelle Garcia <thelionpear@gmail.com>
2018-05-15 19:21:51 -0600

    win or lose pop-up box works now and shows user and computer choice as images

--- js/main.js
+++ js/main.js
@@ -8,8 +8,99 @@
     var tie_tally = 0 
 
 
+    function user_selection(user_input) {
+        var computer_array = ["rock", "paper", "scissors"];
+        var computer_choice = computer_array[Math.floor(Math.random() * computer_array.length)];
+        // switch statement
+        if (user_input == computer_choice) {
+            tie_tally += 1
+            $("#ties").text("Ties:" + " " + tie_tally)
+
+            $("#results_box").show()
+            .css({
+                "background-color": "yellow"
+            })
+            $("#win_or_lose").text("TIE!")
+            
+            
+        } else if (user_input == "rock" && computer_choice == "paper") {
+            losses_tally += 1;
+            $("#losses").text("Losses:" + " " + losses_tally)
+            $("#results_box").show()
+            .css({
+                "background-color": "red"
+            })
+            $("#win_or_lose").text("Lose!")
+            
+            
+        } else if (user_input == "rock" && computer_choice == "scissors") {
+            player_tally += 1; 
+            $("#wins").text("Wins:" + " " + player_tally)
+            $("#results_box").show()
+            .css({
+                "background-color": "green"
+            }) 
+            $("#win_or_lose").text("Win!")
+            
+            
+        } else if (user_input == "paper" && computer_choice == "rock") {
+            player_tally += 1;
+            $("#wins").text("Wins:" + " " + player_tally) 
+            $("#results_box").show()
+            .css({
+                "background-color": "green"
+            })
+            $("#win_or_lose").text("Win!")
+        
+            
+        } else if (user_input == "paper" && computer_choice == "scissors") {
+            losses_tally += 1;
+            $("#losses").text("Losses:" + " " + losses_tally)
+        
+            $("#results_box").show()
+            .css({
+                "background-color": "red"
+            })
+            $("#win_or_lose").text("Lose!")
+            
+        } else if (user_input == "scissors" && computer_choice == "paper") {
+            player_tally += 1; 
+            $("#wins").text("Wins:" + " " + player_tally) 
+            $("#results_box").show()
+            .css({
+                "background-color": "green"
+            }) 
+            $("#win_or_lose").text("Win!")
+            
+        } else if (user_input == "scissors" && computer_choice == "rock") {
+            losses_tally += 1;
+            $("#losses").text("Losses:" + " " + losses_tally)
+            $("#results_box").show()
+            .css({
+                "background-color": "red"
+            })
+            $("#win_or_lose").text("Lose!")
+        
+        }
+
+        add_imgs(user_input, '#player_image');
+        add_imgs(computer_choice, '#computer_image'); 
+        
+    }
+
+    function add_imgs(image_name, element_name) {
+        var img = $("<img />", { 
+            class: "result_box_img",
+            src: image_name + ".png",
+        });
+
+        img.appendTo($(element_name));
+
+    }
+
     $("#continue_playing").click(function() {
-        $("#results_box").hide()
+        $("#results_box").hide();
+        $(".result_box_img").remove() 
     });
 
     $("#new_game").click(function() {
@@ -22,115 +113,6 @@
         $("#ties").text("Ties:" + " " + tie_tally)
         
     });
-
-    function user_selection(user_input) {
-        var computer_array = ["rock", "paper", "scissor"];
-        var computer_choice = computer_array[Math.floor(Math.random() * computer_array.length)];
-        // switch statement
-        if (user_input == computer_choice) {
-            // alert ("You chose "+ user_input + " " + "Computer chose " + " " + computer_choice + "\nIt's a tie!");
-            tie_tally += 1
-            $("#ties").text("Ties:" + " " + tie_tally)
-
-            $("#results_box").show()
-            .css({
-                "background-color": "yellow"
-            })
-            $("#winner_image").text("Player Choice:" + user_input)
-            $("#computer_image").text("Computer_choice:" + computer_choice) 
-            $("#win_or_lose").text("TIE!")
-            
-            
-        } else if (user_input == "rock" && computer_choice == "paper") {
-            // alert ("You chose "+ user_input + " " + "Computer chose " + " " + computer_choice + "\nYou lose!");
-            losses_tally += 1;
-            $("#losses").text("Losses:" + " " + losses_tally)
-            // $("#ties").text("Ties:" + "" + tie_tally)
-
-            $("#results_box").show()
-            .css({
-                "background-color": "red"
-            })
-            $("#winner_image").text("Player Choice:" + user_input)
-            $("#computer_image").text("Computer_choice:" + computer_choice) 
-            $("#win_or_lose").text("Lose!")
-            
-            
-        } else if (user_input == "rock" && computer_choice == "scissors") {
-            // alert ("You chose "+ user_input + " " + "Computer chose " + " " + computer_choice + "\nYou win!");
-            player_tally += 1; 
-            $("#wins").text("Wins:" + " " + player_tally)
-            // $("#ties").text("Ties:" + "" + tie_tally)
-
-            $("#results_box").show()
-            .css({
-                "background-color": "green"
-            })
-            $("#winner_image").text("Player Choice:" + user_input)
-            $("#computer_image").text("Computer_choice:" + computer_choice)  
-            $("#win_or_lose").text("Win!")
-            
-            
-        } else if (user_input == "paper" && computer_choice == "rock") {
-            // alert ("You chose "+ user_input + " " + "Computer chose " + " " + computer_choice + "\nYou win!");
-            player_tally += 1;
-            $("#wins").text("Wins:" + " " + player_tally) 
-            // $("#ties").text("Ties:" + "" + tie_tally)
-
-            $("#results_box").show()
-            .css({
-                "background-color": "green"
-            })
-            $("#winner_image").text("Player Choice:" + user_input)
-            $("#computer_image").text("Computer_choice:" + computer_choice) 
-            $("#win_or_lose").text("Win!")
-        
-            
-        } else if (user_input == "paper" && computer_choice == "scissors") {
-            // alert ("You chose "+ user_input + " " + "Computer chose " + " " + computer_choice + "\nYou lose!");
-            losses_tally += 1;
-            // $("#wins").text("Wins:" + "" + player_tally)
-            $("#losses").text("Losses:" + " " + losses_tally)
-        
-            $("#results_box").show()
-            .css({
-                "background-color": "red"
-            })
-            $("#winner_image").text("Player Choice:" + user_input)
-            $("#computer_image").text("Computer_choice:" + computer_choice) 
-            $("#win_or_lose").text("Lose!")
-            
-        } else if (user_input == "scissors" && computer_choice == "paper") {
-            // alert ("You chose "+ user_input + " " + "Computer chose " + " " + computer_choice + "\nYou win!");
-            player_tally += 1; 
-            $("#wins").text("Wins:" + " " + player_tally) 
-            // $("#ties").text("Ties:" + "" + tie_tally)
-
-            $("#results_box").show()
-            .css({
-                "background-color": "green"
-            })
-            $("#winner_image").text("Player Choice:" + user_input)
-            $("#computer_image").text("Computer_choice:" + computer_choice)  
-            $("#win_or_lose").text("Win!")
-            
-        } else if (user_input == "scissors" && computer_choice == "rock") {
-            // alert ("You chose "+ user_input + " " + "Computer chose " + " " + computer_choice + "\nYou lose!");
-            losses_tally += 1;
-            $("#losses").text("Losses:" + " " + losses_tally)
-            // $("#wins").text("Wins:" + "" + player_tally) 
-            // $("#ties").text("Ties:" + "" + tie_tally)
-
-            $("#results_box").show()
-            .css({
-                "background-color": "red"
-            })
-            $("#winner_image").text("Player Choice:" + user_input)
-            $("#computer_image").text("Computer_choice:" + computer_choice) 
-            $("#win_or_lose").text("Lose!")
-        
-        }
-    }
 
 
 // })
